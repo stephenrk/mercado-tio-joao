69dcac213c3f727b64287b33e3b12b055b4bf67e
stephen.knupfer <stephen.knupfer@verity.com.br>
2017-10-25 13:48:44 -0200

    Correcao de build Service

--- web/src/main/resources/db/migration/V01__criar_tabela_produtos.sql
+++ web/src/main/resources/db/migration/V01__criar_tabela_produtos.sql
@@ -1,8 +1,9 @@
 CREATE TABLE produtos (
-	id BIGINT PRIMARY KEY AUTO_INCREMENT,
-	nome varchar(20)
-);
+	id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+	nome varchar(20),
+	descricao varchar(500)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO produtos (nome) VALUES ('teste1');
-INSERT INTO produtos (nome) VALUES ('teste2');
-INSERT INTO produtos (nome) VALUES ('teste3');
+INSERT INTO produtos (nome, descricao) VALUES ('Suco de uva', '');
+INSERT INTO produtos (nome, descricao) VALUES ('Pão na chapa', '');
+INSERT INTO produtos (nome, descricao) VALUES ('teste3', '');
